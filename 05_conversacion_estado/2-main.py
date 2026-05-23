4d63533fee1698df8ec0ef983c3cb0dd2479bddb
RafaDev-Code <rafaeldelvalledecatamarca@gmail.com>
2026-05-23 19:35:49 -0300

    Completa pieza 05 conversacion con estado

--- 05_conversacion_estado/2-main.py
+++ 05_conversacion_estado/2-main.py
@@ -11,3 +11,26 @@ Verificacion:
 """
 
 # Escribi tu solucion aca.
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+model = os.getenv("OPENAI_MODEL", "gpt-5-mini")
+
+response1 = client.responses.create(
+    model=model,
+    input="Estoy construyendo un gestor de stock"
+)
+
+print("Respuesta 1: ", response1.output_text)
+
+response2 = client.responses.create(
+    model=model,
+    previous_response_id=response1.id,
+    input="Proponme 3 campos para una venta",
+)
+
+print("Respuesta 2: ", response2.output_text)
